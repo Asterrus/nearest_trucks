8fa78c0c9bbfef623df46df0b252c7057214adf6
Asterrus <gastursolar@gmail.com>
2023-05-26 17:43:21 +0500

    crud truck bug fix

--- database/crud/truck.py
+++ database/crud/truck.py
@@ -1,9 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from database.models import Truck
+from database.models import Truck, Location
 
 
-async def update(db: AsyncSession, location_id: int, truck: Truck) -> Truck:
-    truck.location_id = location_id
+async def update(db: AsyncSession, location: Location, truck: Truck) -> Truck:
+    truck.location_id = location.id
     await db.commit()
     return truck
